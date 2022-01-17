e4c7aec1afe57d34a0f1ed7ad2fa128b97132077
quincinia <eanj13@gmail.com>
2022-01-17 09:26:02 -0800

    Finished 5.1

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Submissions for FSO part 
 
 All exercises will be on the same branch so I can get my contribution squares
+
+## Exercise 5.1
+Followed the story closely. User cannot log out unless the app is restarted.
--- src/App.js
+++ src/App.js
@@ -1,37 +1,58 @@
 import React, { useState, useEffect } from "react"
 import Blog from "./components/Blog"
 import blogService from "./services/blogs"
+import loginService from "./services/login"
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
 
     useEffect(() => {
         blogService.getAll().then((blogs) => setBlogs(blogs))
     }, [])
 
     const handleLogin = async (event) => {
+        event.preventDefault()
 
+        try {
+            const user = await loginService.login({
+                username, password
+            })
+            // After successful login, update user and clear form
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (exception) {
+            // Display notification
+        }
     }
+
     const loginForm = () => (
-        <form onSubmit={handleLogin}>
+        <div>
             <h2>log in to application</h2>
-            <div>
-                username
-                <input type="text" value={username} onChange={({ target }) => setUsername(target)}/>
-            </div>
-            <div>
-                password
-                <input type="text" value={password} onChange={({ target }) => setPassword(target)}/>
-            </div>
-            <button type="submit">login</button>
-        </form>
+            <form onSubmit={handleLogin}>
+                <div>
+                    username
+                    <input type="text" name="Username" value={username} onChange={({ target }) => setUsername(target.value)}/>
+                </div>
+                <div>
+                    password
+                    <input type="password" name="Password" value={password} onChange={({ target }) => setPassword(target.value)}/>
+                </div>
+                <button type="submit">login</button>
+            </form>
+        </div>
     )
+
+    if (user === null) {
+        return loginForm()
+    }
     return (
         <div>
-            {loginForm()}
             <h2>blogs</h2>
+            <p>{user.name} logged in</p>
             {blogs.map((blog) => (
                 <Blog key={blog.id} blog={blog} />
             ))}
