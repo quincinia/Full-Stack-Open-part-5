df493c4358dd696727c1631277ff38a406eb70f3
quincinia <eanj13@gmail.com>
2022-01-19 14:13:21 -0800

    Finished 5.7

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ Added Notification component from previous parts. Added CSS file for coloring.
 
 ## Exercise 5.5
 Followed the story closely, but had a few small issues (syntax errors, Togglable not liking the array).
+
+## Exercise 5.6
+BlogForm was extracted into its own component during 5.5.
+
+## Exercise 5.7
+Blogs can now be expanded. Placed a like button, but it isn't working yet. Didn't wrap the blog title and author in a `<div>` like in the example.
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({blog}) => {
+    const [visible, setVisible] = useState(false)
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+
+    return (
+        <div style={blogStyle}>
+            {blog.title} {blog.author}
+            <button onClick={toggleVisibility}>{visible ? 'hide' : 'show'}</button>
+            <div style={ { display: visible ? '' : 'none' } }>
+                {blog.url}<br />
+                likes {blog.likes}<button>like</button><br />
+                {blog.author}
+            </div>
+        </div>
+    )
+}
 
 export default Blog
